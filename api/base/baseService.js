20bd2ed877046f8ed9f00e50a952083cda70c903
light <978854603@qq.com>
2019-09-25 18:46:24 +0800

    init

--- api/base/baseService.js
+++ api/base/baseService.js
@@ -1,4 +1,17 @@
-class baseModel()
-{
-    
+'use strict';
+const path = require('path');
+const fs = require('fs');
+const db = require('../../middleware/db');
+
+let modelUrl = path.resolve(__dirname, './service/');
+let files = fs.readdirSync(modelUrl);
+
+let js_files = files.filter(f => {
+  return f.endsWith('.js');
+}, files);
+
+for (let f of js_files) {
+  console.log(`import model from file ${f}...`);
+  let name = f.substring(0, f.length - 3);
+  module.exports[name] = require(path.resolve(modelUrl, f));
 }
--- api/service/user.js
+++ api/service/user.js
@@ -1,13 +1,81 @@
-const model = require('../base/baseModel');
-const user = require('../model/user');
-
+const { user } = require('../base/baseModel');
 module.exports = {
   async findOne() {
-    const user = await model.User.findAndCountAll({
+    const user = await user.findOne({
       offset,
       limit,
       order: [['createdAt', 'desc'], ['id', 'desc']]
     });
     return user;
+  },
+
+  async userList(ctx, next) {
+    let offset = ctx.request.query.offset || 0;
+    let limit = ctx.request.query.limit || 10;
+
+    ctx.json({
+      users: user
+    });
+  },
+
+  async users(ctx, next) {
+    let userParam = {
+      name: ctx.request.body.name,
+      password: ctx.request.body.password
+    };
+    let user = await user.create(userParam);
+    ctx.json(user);
+  },
+
+  // 用户注册
+  async register(ctx, next) {
+    let name = ctx.request.body.name;
+    const user = await user.findOne({
+      where: {
+        name: name
+      }
+    });
+
+    if (user) {
+      let resObj = { code: '1001', msg: '用户已存在!' };
+      ctx.json(resObj);
+    } else {
+      let userParam = {
+        name: name,
+        password: md5.getMd5(ctx.request.body.password)
+      };
+      let newUser = await user.create(userParam);
+      if (newUser) {
+        let resObj = { code: '200', msg: `${name}注册成功!` };
+        ctx.json(resObj);
+      }
+    }
+  },
+
+  // 用户登录
+  async login(ctx, next) {
+    let name = ctx.request.body.name;
+    const user = await user.findOne({
+      where: {
+        name: name
+      }
+    });
+
+    if (!user) {
+      let resObj = { code: '1002', msg: '用户不存在!' };
+      ctx.json(resObj);
+    } else {
+      if (user.password !== md5.getMd5(ctx.request.body.password)) {
+        let resObj = { code: '1003', msg: '密码错误!' };
+        ctx.json(resObj);
+      } else {
+        // token处理：token存储在redis且返回
+        let token = `${user.id}-${Date.now()}`;
+        let tokenObj = { userId: user.id, expires: config.expires, creatTime: Date.now() };
+        ctx.redisClient.hmset(token, tokenObj);
+        let resObj = { code: '200', token: token };
+        ctx.json(resObj);
+      }
+    }
   }
 };
